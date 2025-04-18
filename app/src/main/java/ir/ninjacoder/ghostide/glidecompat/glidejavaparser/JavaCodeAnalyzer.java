4ba435383230666f37fed5860ce34279d4f6af5c
Akram Khan <akrammiru@gmail.com>
2025-04-18 17:47:20 +0600

    remove unused java class

--- app/src/main/java/ir/ninjacoder/ghostide/glidecompat/glidejavaparser/JavaCodeAnalyzer.java
+++
@@ -1,9 +0,0 @@
-package ir.ninjacoder.ghostide.glidecompat.glidejavaparser;
-
-import java.io.File;
-import java.io.IOException;
-
-public interface JavaCodeAnalyzer {
-
-    JavaAnalysisResult analyze(File javaFile) throws IOException;
-}
--- app/src/main/java/ir/ninjacoder/ghostide/marco/data.groovy
+++
@@ -1,5 +0,0 @@
-package Ninja.coder.Ghostemane.code.marco;
-
-public class data {
-
-}
