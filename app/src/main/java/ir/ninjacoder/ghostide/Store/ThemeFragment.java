44c8805109e895e2da995c52342e8254e88d616e
HanzoDev1375 <ninjacodermain1375@gmail.com>
2025-06-25 11:20:40 +0330

    test

--- app/src/main/java/ir/ninjacoder/ghostide/Store/ThemeFragment.java
+++ app/src/main/java/ir/ninjacoder/ghostide/Store/ThemeFragment.java
@@ -1,6 +1,5 @@
 package ir.ninjacoder.ghostide.Store;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.widget.Toast;
@@ -12,6 +11,7 @@ import java.io.File;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.os.Bundle;
+import android.app.*;
 import android.view.View;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
@@ -149,13 +149,24 @@ public class ThemeFragment extends Fragment {
       }
     }
   }
+	
+	class Sheet extends CustomSheet {
+    public Sheet(Context c) {
+      super(c);
+    }
+
+    @Override
+    public View getView() {
+      return previewBinding.getRoot();
+    }
+  }
+
 
   private void showThemePreview(Map<String, String> themeData) {
     previewBinding = LayoutCustomimagepreviewBinding.inflate(LayoutInflater.from(getContext()));
 
-    CustomSheet sheet = new CustomSheet(getContext());
+    Sheet sheet = new Sheet(getContext());
     sheet.setFullScreen();
-    sheet.setView(previewBinding.getRoot());
     sheet.show();
 
     previewBinding.fab1.setVisibility(View.INVISIBLE);
@@ -177,12 +188,12 @@ public class ThemeFragment extends Fragment {
   }
 
   private void downloadAndSaveTheme(Map<String, String> themeData) {
-    AlertDialog progressDialog =
-        new MaterialAlertDialogBuilder(getContext())
+    var progressDialog =
+        new MaterialAlertDialogBuilder(getContext()).create()
             .setTitle("در حال دانلود")
             .setMessage("لطفاً صبر کنید...")
             .setCancelable(false)
-            .create();
+            ;
 
     progressDialog.show();
 
@@ -219,6 +230,7 @@ public class ThemeFragment extends Fragment {
             progressDialog.dismiss();
             if (success) {
               showInstallDialog(themeData);
+
             } else {
               showToast("خطا در ذخیره برخی فایل ها");
             }
